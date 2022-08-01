be506d1551fa9a28a32a1d44e088c993b802d92a
tombch <tombrier@hotmail.co.uk>
2022-08-01 15:25:03 +0100

    updated responses

--- utils/responses.py
+++ utils/responses.py
@@ -8,10 +8,10 @@ class Responses:
 
     # Bad request
     _400_no_institute = Response({"detail" : "no institute was provided"}, status=status.HTTP_400_BAD_REQUEST)
-    _400_pathogen_code_in_url_only = Response({"detail" : "pathogen_code is provided in the request url only, not in the request body"}, status=status.HTTP_400_BAD_REQUEST)
+    _400_cannot_provide_cid = Response({"detail" : "cids are generated internally and are not provided in the request"}, status=status.HTTP_400_BAD_REQUEST)
+    _400_mismatch_pathogen_code = Response({"detail" : "pathogen_code provided in request body does not match pathogen code in url"}, status=status.HTTP_400_BAD_REQUEST)
 
     # Forbidden
     _403_different_institute = Response({"detail" : "cannot approve this user. they belong to a different institute"}, status=status.HTTP_403_FORBIDDEN)
-    _403_cannot_provide_cid = Response({"detail" : "cids are generated internally and cannot be provided"}, status=status.HTTP_403_FORBIDDEN)
     _403_incorrect_institute_for_user = Response({"detail" : "user cannot create/modify data for this institute"}, status=status.HTTP_403_FORBIDDEN)
     _403_cannot_query_id = Response({"detail" : "id fields cannot be queried"}, status=status.HTTP_403_FORBIDDEN)
