61f1fe3a126f22791131efce3370e0523fb648a0
tombch <tombrier@hotmail.co.uk>
2022-08-03 17:50:13 +0100

    added validation errors

--- utils/fieldserializers.py
+++ utils/fieldserializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 from datetime import date
 
 
 class YearMonthField(serializers.Field):
     def to_representation(self, value):
-        year, month, _ = str(value).split("-")
+        try:
+            year, month, _ = str(value).split("-")
+        except ValueError as e:
+            raise ValidationError("must be in YYYY-WW-DD format")
+
         return year + "-" + month
 
     def to_internal_value(self, data):
-        year, month = str(data).split("-")
-        value = date(int(year), int(month), 1)
+        try:
+            year, month = str(data).split("-")
+        except ValueError as e:
+            raise ValidationError("must be in YYYY-WW format")
+        
+        try:
+            value = date(int(year), int(month), 1)
+        except ValueError as e:
+            raise ValidationError(e)
+        
         return value
