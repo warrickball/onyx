59cc8fcc8861b9bac6140c04a2c9c187adf752e4
tombch <tombrier@hotmail.co.uk>
2022-09-22 13:57:57 +0100

    turned off auto reload

--- metadb/gunicorn.conf.py
+++ metadb/gunicorn.conf.py
@@ -4,6 +4,6 @@ import multiprocessing
 
 bind = "127.0.0.1:8000"
 loglevel = "DEBUG"
-workers = multiprocessing.cpu_count() * 2 + 1  # TODO: Experiment
+workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 120
-reload = True  # TODO: Development only?
+reload = False
