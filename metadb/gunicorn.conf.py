036d999c5c0dc6a68423a6b907e0b3d62f25b2e5
tombch <tombrier@hotmail.co.uk>
2022-11-09 11:30:33 +0000

    updated gunicorn config file

--- metadb/gunicorn.conf.py
+++ metadb/gunicorn.conf.py
@@ -1,12 +1,11 @@
 """gunicorn WSGI server configuration."""
 
-import multiprocessing
 import os
 
-bind = "127.0.0.1:8000"
-loglevel = "DEBUG"
-workers = os.getenv("METADB_WORKER_COUNT")
-timeout = 120
-reload = True
-# keyfile = os.getenv("METADB_KEYFILE")
-# certfile = os.getenv("METADB_CERTFILE")
+bind = f"{os.environ['METADB_HOST']}:{os.environ['METADB_PORT']}"
+workers = os.environ["METADB_WORKER_COUNT"]
+loglevel = os.environ["METADB_LOG_LEVEL"]
+timeout = os.environ["METADB_WORKER_TIMEOUT"]
+
+# keyfile = os.environ["METADB_KEYFILE"]
+# certfile = os.environ["METADB_CERTFILE"]
