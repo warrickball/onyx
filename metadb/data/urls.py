8dbb6e8bb0b6d977c2b007bdc0fcafb15775894d
tombch <tombrier@hotmail.co.uk>
2023-02-06 15:55:52 +0000

    changed project to project_code

--- metadb/data/urls.py
+++ metadb/data/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("projects/", views.ProjectView.as_view()),
-    path("create/<project>/", views.CreateProjectItemView.as_view()),
-    path("get/<project>/", views.GetProjectItemView.as_view()),
-    path("query/<project>/", views.QueryProjectItemView.as_view()),
-    path("update/<project>/<cid>/", views.UpdateProjectItemView.as_view()),
-    path("suppress/<project>/<cid>/", views.SuppressProjectItemView.as_view()),
-    path("delete/<project>/<cid>/", views.DeleteProjectItemView.as_view()),
-    # path("testcreate/<project>/", views.TestCreateProjectItemView.as_view()),
-    # path("testupdate/<project>/", views.TestUpdateProjectItemView.as_view()),
+    path("create/<project_code>/", views.CreateRecordView.as_view()),
+    path("get/<project_code>/", views.GetRecordView.as_view()),
+    path("query/<project_code>/", views.QueryRecordView.as_view()),
+    path("update/<project_code>/<cid>/", views.UpdateRecordView.as_view()),
+    path("suppress/<project_code>/<cid>/", views.SuppressRecordView.as_view()),
+    path("delete/<project_code>/<cid>/", views.DeleteRecordView.as_view()),
+    # path("testcreate/<project_code>/", views.TestCreateRecordView.as_view()),
+    # path("testupdate/<project_code>/", views.TestUpdateRecordView.as_view()),
 ]
