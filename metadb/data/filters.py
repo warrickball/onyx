069b2934e1af8aed0c1b803a423bce875fba46ff
tombch <tombrier@hotmail.co.uk>
2023-02-16 19:17:42 +0000

    yearmonth grouped with other date fields

--- metadb/data/filters.py
+++ metadb/data/filters.py
@@ -31,6 +31,7 @@ BASE_LOOKUPS = [
     "gte",
 ]
 
+# Text field types
 TEXT_FIELDS = [
     LowerCharField,
     models.CharField,
@@ -50,12 +51,15 @@ TEXT_LOOKUPS = [
     "iregex",
 ]
 
+# Number field types
 NUMBER_FIELDS = [
     models.IntegerField,
     models.FloatField,
 ]
 
+# Date field types
 DATE_FIELDS = [
+    YearMonthField,
     models.DateField,
     models.DateTimeField,
 ]
@@ -67,7 +71,7 @@ YEARMONTH_LOOKUPS = [
     "year__range",
 ]
 
-# Additional lookups for date and datetime fields
+# Additional lookups for other date fields
 DATE_LOOKUPS = YEARMONTH_LOOKUPS + [
     "iso_year",
     "iso_year__in",
@@ -83,7 +87,7 @@ BOOLEAN_CHOICES = (
     ("false", "false"),
 )
 
-
+# Mappings from field type + lookup to filter
 FILTERS = {
     ("choice", "in"): ModelChoiceInFilter,
     ("choice", "range"): ModelChoiceRangeFilter,
@@ -211,62 +215,40 @@ def get_filter(model, user_field, fields):
                 coerce=strtobool,
                 lookup_expr="isnull",
             )
-    # Yearmonth
-    elif field_type == YearMonthField:
-        if not lookup:
-            return f"{field}", filters.DateFilter(
-                field_name=field_path,
-                input_formats=["%Y-%m"],
-            )
-        elif lookup in ["in", "range"] or lookup in BASE_LOOKUPS:
-            filter = FILTERS.get(("date", lookup), filters.DateFilter)
-            return f"{field}__{lookup}", filter(
-                field_name=field_path,
-                input_formats=["%Y-%m"],
-                lookup_expr=lookup,
-            )
-        elif lookup in YEARMONTH_LOOKUPS:
-            if lookup == "year__in":
-                filter = NumberInFilter
-            elif lookup == "year__range":
-                filter = NumberRangeFilter
-            else:
-                filter = filters.NumberFilter
-            return f"{field}__{lookup}", filter(
-                field_name=field_path,
-                lookup_expr=lookup,
-            )
-        elif lookup == "isnull":
-            return f"{field}__isnull", filters.TypedChoiceFilter(
-                field_name=field_path,
-                choices=BOOLEAN_CHOICES,
-                coerce=strtobool,
-                lookup_expr="isnull",
-            )
     # Date
     elif field_type in DATE_FIELDS:
-        if field_type == models.DateField:
+        if field_type == YearMonthField:
             filter = filters.DateFilter
             filter_type = "date"
+            lookups = YEARMONTH_LOOKUPS
+            input_formats = ["%Y-%m"]
+        elif field_type == models.DateField:
+            filter = filters.DateFilter
+            filter_type = "date"
+            lookups = DATE_LOOKUPS
+            input_formats = ["%Y-%m-%d"]
         else:
             filter = filters.DateTimeFilter
             filter_type = "datetime"
+            lookups = DATE_LOOKUPS
+            input_formats = ["%Y-%m-%d"]
+
         if not lookup:
             return f"{field}", filter(
                 field_name=field_path,
-                input_formats=["%Y-%m-%d"],
+                input_formats=input_formats,
             )
         elif lookup in ["in", "range"] or lookup in BASE_LOOKUPS:
             filter = FILTERS.get((filter_type, lookup), filter)  # type: ignore
             return f"{field}__{lookup}", filter(
                 field_name=field_path,
-                input_formats=["%Y-%m-%d"],
+                input_formats=input_formats,
                 lookup_expr=lookup,
             )
-        elif lookup in DATE_LOOKUPS:
-            if lookup in ["year__in", "iso_year__in", "week__in"]:
+        elif lookup in lookups:
+            if lookup.endswith("__in"):
                 filter = NumberInFilter
-            elif lookup in ["year__range", "iso_year__range", "week__range"]:
+            elif lookup.endswith("__range"):
                 filter = NumberRangeFilter
             else:
                 filter = filters.NumberFilter
@@ -319,292 +301,3 @@ class METADBFilter(filters.FilterSet):
             )
             if name:
                 self.filters[name] = filter
-
-
-# class METADBFilter(filters.FilterSet):
-#     def __init__(self, model, fields, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-
-#         for field in fields:
-#             if not any(d.startswith(field) for d in self.data):
-#                 continue
-
-#             model_field = model._meta.get_field(field)
-
-#             if isinstance(model_field, ForeignKey):
-#                 related_field = model_field.foreign_related_fields[0].name  # type: ignore
-#                 field_type = type(
-#                     model_field.related_model._meta.get_field(related_field)
-#                 )
-#                 field_path = field + "__" + related_field
-
-#             else:
-#                 related_field = field
-#                 field_type = type(model_field)
-#                 field_path = field
-
-#             if (
-#                 field in model.CustomMeta.db_choice_fields
-#                 or model_field.related_model == Choice
-#             ):
-#                 if field in model.CustomMeta.db_choice_fields:
-#                     db_model = model._meta.get_field(field).related_model
-#                     qs = db_model.objects.all()
-#                     to_field_name = related_field
-#                 else:
-#                     content_type = ContentType.objects.get_for_model(model)
-#                     qs = Choice.objects.filter(
-#                         content_type=content_type,
-#                         field=field,
-#                     )
-#                     to_field_name = "choice"
-#                     field_type = LowerCharField
-#                     field_path = field + "__choice"
-
-#                 self.filters[field] = filters.ModelChoiceFilter(
-#                     field_name=field,
-#                     queryset=qs,
-#                     to_field_name=to_field_name,
-#                 )
-#                 self.filters[f"{field}__in"] = ModelChoiceInFilter(
-#                     field_name=field,
-#                     queryset=qs,
-#                     to_field_name=to_field_name,
-#                     lookup_expr="in",
-#                 )
-#                 self.filters[f"{field}__range"] = ModelChoiceRangeFilter(
-#                     field_name=field,
-#                     queryset=qs,
-#                     to_field_name=to_field_name,
-#                     lookup_expr="range",
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.ModelChoiceFilter(
-#                         field_name=field,
-#                         queryset=qs,
-#                         to_field_name=to_field_name,
-#                         lookup_expr=lookup,
-#                     )
-
-#                 if field_type in [LowerCharField, models.CharField, models.TextField]:
-#                     for lookup in TEXT_LOOKUPS:
-#                         self.filters[f"{field}__{lookup}"] = filters.CharFilter(
-#                             field_name=field_path, lookup_expr=lookup
-#                         )
-
-#             elif field_type in [LowerCharField, models.CharField, models.TextField]:
-#                 self.filters[field] = filters.CharFilter(field_name=field_path)
-#                 self.filters[f"{field}__in"] = CharInFilter(
-#                     field_name=field_path, lookup_expr="in"
-#                 )
-#                 self.filters[f"{field}__range"] = CharRangeFilter(
-#                     field_name=field_path, lookup_expr="range"
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.CharFilter(
-#                         field_name=field_path, lookup_expr=lookup
-#                     )
-
-#                 for lookup in TEXT_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.CharFilter(
-#                         field_name=field_path, lookup_expr=lookup
-#                     )
-
-#             elif field_type in [models.IntegerField, models.FloatField]:
-#                 self.filters[field] = filters.NumberFilter(field_name=field_path)
-
-#                 self.filters[f"{field}__in"] = NumberInFilter(
-#                     field_name=field_path, lookup_expr="in"
-#                 )
-#                 self.filters[f"{field}__range"] = NumberRangeFilter(
-#                     field_name=field_path, lookup_expr="range"
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.NumberFilter(
-#                         field_name=field_path, lookup_expr=lookup
-#                     )
-
-#             elif field_type == YearMonthField:
-#                 self.filters[field] = filters.DateFilter(
-#                     field_name=field_path, input_formats=["%Y-%m"]
-#                 )
-#                 self.filters[f"{field}__in"] = DateInFilter(
-#                     field_name=field_path,
-#                     input_formats=["%Y-%m"],
-#                     lookup_expr="in",
-#                 )
-#                 self.filters[f"{field}__range"] = DateRangeFilter(
-#                     field_name=field_path, input_formats=["%Y-%m"], lookup_expr="range"
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-#                 self.filters[f"{field}__iso_year"] = filters.NumberFilter(
-#                     field_name=field_path, lookup_expr="iso_year"
-#                 )
-#                 self.filters[f"{field}__iso_year__in"] = NumberInFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__in",
-#                 )
-#                 self.filters[f"{field}__iso_year__range"] = NumberRangeFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__range",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.DateFilter(
-#                         field_name=field_path,
-#                         input_formats=["%Y-%m"],
-#                         lookup_expr=lookup,
-#                     )
-
-#             elif field_type == models.DateField:
-#                 self.filters[field] = filters.DateFilter(
-#                     field_name=field_path, input_formats=["%Y-%m-%d"]
-#                 )
-#                 self.filters[f"{field}__in"] = DateInFilter(
-#                     field_name=field_path, input_formats=["%Y-%m-%d"], lookup_expr="in"
-#                 )
-#                 self.filters[f"{field}__range"] = DateRangeFilter(
-#                     field_name=field_path,
-#                     input_formats=["%Y-%m-%d"],
-#                     lookup_expr="range",
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-#                 self.filters[f"{field}__iso_year"] = filters.NumberFilter(
-#                     field_name=field_path, lookup_expr="iso_year"
-#                 )
-#                 self.filters[f"{field}__iso_year__in"] = NumberInFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__in",
-#                 )
-#                 self.filters[f"{field}__iso_year__range"] = NumberRangeFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__range",
-#                 )
-#                 self.filters[f"{field}__week"] = filters.NumberFilter(
-#                     field_name=field_path, lookup_expr="week"
-#                 )
-#                 self.filters[f"{field}__week__in"] = NumberInFilter(
-#                     field_name=field_path,
-#                     lookup_expr="week__in",
-#                 )
-#                 self.filters[f"{field}__week__range"] = NumberRangeFilter(
-#                     field_name=field_path,
-#                     lookup_expr="week__range",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.DateFilter(
-#                         field_name=field_path,
-#                         input_formats=["%Y-%m-%d"],
-#                         lookup_expr=lookup,
-#                     )
-
-#             elif field_type == models.DateTimeField:
-#                 self.filters[field] = filters.DateTimeFilter(
-#                     field_name=field_path, input_formats=["%Y-%m-%d"]
-#                 )
-#                 self.filters[f"{field}__in"] = DateTimeInFilter(
-#                     field_name=field_path, input_formats=["%Y-%m-%d"], lookup_expr="in"
-#                 )
-#                 self.filters[f"{field}__range"] = DateTimeRangeFilter(
-#                     field_name=field_path,
-#                     input_formats=["%Y-%m-%d"],
-#                     lookup_expr="range",
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-#                 self.filters[f"{field}__iso_year"] = filters.NumberFilter(
-#                     field_name=field_path, lookup_expr="iso_year"
-#                 )
-#                 self.filters[f"{field}__iso_year__in"] = NumberInFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__in",
-#                 )
-#                 self.filters[f"{field}__iso_year__range"] = NumberRangeFilter(
-#                     field_name=field_path,
-#                     lookup_expr="iso_year__range",
-#                 )
-#                 self.filters[f"{field}__week"] = filters.NumberFilter(
-#                     field_name=field_path, lookup_expr="week"
-#                 )
-#                 self.filters[f"{field}__week__in"] = NumberInFilter(
-#                     field_name=field_path,
-#                     lookup_expr="week__in",
-#                 )
-#                 self.filters[f"{field}__week__range"] = NumberRangeFilter(
-#                     field_name=field_path,
-#                     lookup_expr="week__range",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.DateTimeFilter(
-#                         field_name=field_path,
-#                         input_formats=["%Y-%m-%d"],
-#                         lookup_expr=lookup,
-#                     )
-
-#             elif field_type == models.BooleanField:
-#                 self.filters[field] = filters.TypedChoiceFilter(
-#                     field_name=field_path, choices=BOOLEAN_CHOICES, coerce=strtobool
-#                 )
-#                 self.filters[f"{field}__in"] = TypedChoiceInFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="in",
-#                 )
-#                 self.filters[f"{field}__range"] = TypedChoiceRangeFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="range",
-#                 )
-#                 self.filters[f"{field}__isnull"] = filters.TypedChoiceFilter(
-#                     field_name=field_path,
-#                     choices=BOOLEAN_CHOICES,
-#                     coerce=strtobool,
-#                     lookup_expr="isnull",
-#                 )
-
-#                 for lookup in BASE_LOOKUPS:
-#                     self.filters[f"{field}__{lookup}"] = filters.TypedChoiceFilter(
-#                         field_name=field_path,
-#                         choices=BOOLEAN_CHOICES,
-#                         coerce=strtobool,
-#                         lookup_expr=lookup,
-#                     )
