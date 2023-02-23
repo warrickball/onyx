0940c56270fb9deec9908c40b74bec040935e97e
tombch <tombrier@hotmail.co.uk>
2023-02-23 13:19:17 +0000

    updated signals and tasks

--- metadb/data/signals.py
+++ metadb/data/signals.py
@@ -4,16 +4,9 @@ from data.models import Mpx
 from internal.models import Signal
 from django.utils import timezone
 
-# from data.tasks import generate_fasta_statistics
-
 
 @receiver(post_save, sender=Mpx)
 def post_save_mpx(sender, instance, **kwargs):
     signal, created = Signal.objects.get_or_create(code="mpx")
     signal.modified = timezone.now()
     signal.save(update_fields=["modified"])
-
-    # if not instance.fasta_statistics:
-    #     generate_fasta_statistics.delay(
-    #         cid=instance.cid, fasta_path=instance.fasta_path
-    #     )
