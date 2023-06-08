79bed09a05d87709d2a1a2d9b16a7d039ec8557b
tombch <t.brier@outlook.com>
2023-06-08 16:40:02 +0100

    tracking old signal and task code before it gets moved to project-specific subdir, at a later point

--- metadb/data/tasks.py
+++ metadb/data/tasks.py
@@ -1,96 +1,105 @@
-from django.utils import timezone
-from data.models import Mpx
-from internal.models import Signal
-from utils.project import (
-    get_project_and_model,
-)
-from utils.permissions import get_fields_from_permissions
-from celery import shared_task
-from .serializers import MpxSerializer
-from cursor_pagination import CursorPaginator
-import csv
-import os
+# from django.utils import timezone
+# from data.models.projects import Mpx
+# from internal.models import Signal
+# from utils.project import (
+#     get_project_and_model,
+# )
+# from celery import shared_task
+# from .serializers import MpxSerializer
+# from cursor_pagination import CursorPaginator
+# import csv
+# import os
 
 
-def paginator(qs, cursor=None, page_size=5000):
-    paginator = CursorPaginator(qs, ordering=("-created", "-id"))
-    page = paginator.page(first=page_size, after=cursor)
-    data = {
-        "objects": page.items,
-        "has_next": page.has_next,
-        "cursor": paginator.cursor(page[-1]),
-    }
-    return data
+# def get_fields_from_permissions(permissions):
+#     return [
+#         field
+#         for (_, _, field) in (
+#             x.partition("__") for x in permissions.values_list("codename", flat=True)
+#         )
+#         if field
+#     ]
 
 
-@shared_task
-def create_mpx_tables():
-    signal, created = Signal.objects.get_or_create(code="mpx")
+# def paginator(qs, cursor=None, page_size=5000):
+#     paginator = CursorPaginator(qs, ordering=("-created", "-id"))
+#     page = paginator.page(first=page_size, after=cursor)
+#     data = {
+#         "objects": page.items,
+#         "has_next": page.has_next,
+#         "cursor": paginator.cursor(page[-1]),
+#     }
+#     return data
 
-    if (not created) and (
-        (timezone.now() - signal.modified).total_seconds()
-        > 60 * int(os.environ["METADB_CELERY_BEAT_TIME"])
-    ):
-        print("no changes detected")
-        return None
 
-    print("changes detected")
+# @shared_task
+# def create_mpx_tables():
+#     signal, created = Signal.objects.get_or_create(code="mpx")
 
-    temp_mpx_file = f"{os.environ['METADB_MPX_TSV']}.temp"
-    final_mpx_file = os.environ["METADB_MPX_TSV"]
-    temp_pha_mpx_file = f"{os.environ['METADB_PHA_MPX_TSV']}.temp"
-    final_pha_mpx_file = os.environ["METADB_PHA_MPX_TSV"]
+#     if (not created) and (
+#         (timezone.now() - signal.modified).total_seconds()
+#         > 60 * int(os.environ["METADB_CELERY_BEAT_TIME"])
+#     ):
+#         print("no changes detected")
+#         return None
 
-    mpx_project, _ = get_project_and_model("mpx")
-    mpx_view_fields = get_fields_from_permissions(
-        mpx_project.view_group.permissions.all()  # type: ignore
-    )
+#     print("changes detected")
 
-    mpxpha_project, _ = get_project_and_model("mpxpha")
-    mpxpha_view_fields = get_fields_from_permissions(
-        mpxpha_project.view_group.permissions.all()  # type: ignore
-    )
+#     temp_mpx_file = f"{os.environ['METADB_MPX_TSV']}.temp"
+#     final_mpx_file = os.environ["METADB_MPX_TSV"]
+#     temp_pha_mpx_file = f"{os.environ['METADB_PHA_MPX_TSV']}.temp"
+#     final_pha_mpx_file = os.environ["METADB_PHA_MPX_TSV"]
 
-    with open(temp_mpx_file, "w") as temp_mpx, open(
-        temp_pha_mpx_file, "w"
-    ) as temp_pha_mpx:
-        mpx_writer = csv.DictWriter(
-            temp_mpx,
-            fieldnames=mpx_view_fields,
-            delimiter="\t",
-        )
-        mpx_writer.writeheader()
+#     mpx_project, _ = get_project_and_model("mpx")
+#     mpx_view_fields = get_fields_from_permissions(
+#         mpx_project.view_group.permissions.all()  # type: ignore
+#     )
 
-        pha_mpx_writer = csv.DictWriter(
-            temp_pha_mpx,
-            fieldnames=mpxpha_view_fields,
-            delimiter="\t",
-        )
-        pha_mpx_writer.writeheader()
+#     mpxpha_project, _ = get_project_and_model("mpxpha")
+#     mpxpha_view_fields = get_fields_from_permissions(
+#         mpxpha_project.view_group.permissions.all()  # type: ignore
+#     )
 
-        cursor = None
-        has_next = True
-        while has_next:
-            data = paginator(Mpx.objects.filter(suppressed=False), cursor=cursor)
+#     with open(temp_mpx_file, "w") as temp_mpx, open(
+#         temp_pha_mpx_file, "w"
+#     ) as temp_pha_mpx:
+#         mpx_writer = csv.DictWriter(
+#             temp_mpx,
+#             fieldnames=mpx_view_fields,
+#             delimiter="\t",
+#         )
+#         mpx_writer.writeheader()
 
-            mpx_serialized = MpxSerializer(
-                data["objects"],
-                many=True,
-                fields=mpx_view_fields,
-            ).data
-            mpx_writer.writerows(mpx_serialized)
+#         pha_mpx_writer = csv.DictWriter(
+#             temp_pha_mpx,
+#             fieldnames=mpxpha_view_fields,
+#             delimiter="\t",
+#         )
+#         pha_mpx_writer.writeheader()
 
-            pha_mpx_serialized = MpxSerializer(
-                data["objects"],
-                many=True,
-                fields=mpxpha_view_fields,
-            ).data
-            pha_mpx_writer.writerows(pha_mpx_serialized)
+#         cursor = None
+#         has_next = True
+#         while has_next:
+#             data = paginator(Mpx.objects.filter(suppressed=False), cursor=cursor)
 
-            cursor = data["cursor"]
-            has_next = data["has_next"]
+#             mpx_serialized = MpxSerializer(
+#                 data["objects"],
+#                 many=True,
+#                 fields=mpx_view_fields,
+#             ).data
+#             mpx_writer.writerows(mpx_serialized)
 
-    os.rename(temp_mpx_file, final_mpx_file)
-    os.rename(temp_pha_mpx_file, final_pha_mpx_file)
+#             pha_mpx_serialized = MpxSerializer(
+#                 data["objects"],
+#                 many=True,
+#                 fields=mpxpha_view_fields,
+#             ).data
+#             pha_mpx_writer.writerows(pha_mpx_serialized)
 
-    print("changes acted on successfully")
+#             cursor = data["cursor"]
+#             has_next = data["has_next"]
+
+#     os.rename(temp_mpx_file, final_mpx_file)
+#     os.rename(temp_pha_mpx_file, final_pha_mpx_file)
+
+#     print("changes acted on successfully")
