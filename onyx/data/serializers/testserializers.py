0de8353435826e7a1b8c5b77707855f1f48e332e
tombch <t.brier@outlook.com>
2023-06-19 15:11:57 +0100

    Added TestModel and TestSerializer for use in tests

---
+++ onyx/data/serializers/testserializers.py
@@ -0,0 +1,45 @@
+from rest_framework.validators import UniqueTogetherValidator
+from ..serializers import RecordSerializer
+from data.models import TestModel
+from utils.fieldserializers import ChoiceField, YearMonthField
+
+
+class TestSerializer(RecordSerializer):
+    collection_month = YearMonthField(required=False, allow_null=True)
+    received_month = YearMonthField(required=False, allow_null=True)
+    country = ChoiceField(TestModel, "country")
+    region = ChoiceField(TestModel, "region")
+
+    class Meta:
+        model = TestModel
+        fields = [
+            "sample_id",
+            "run_name",
+            "collection_month",
+            "received_month",
+            "submission_date",
+            "country",
+            "region",
+            "concern",
+            "tests",
+            "score",
+        ]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=TestModel.objects.all(),
+                fields=["sample_id", "run_name"],
+            )
+        ]
+
+    class ExtraMeta(RecordSerializer.ExtraMeta):
+        optional_value_groups = RecordSerializer.ExtraMeta.optional_value_groups + [
+            ("collection_month", "received_month")
+        ]
+        orderings = RecordSerializer.ExtraMeta.orderings + [
+            ("collection_month", "received_month")
+        ]
+        non_futures = RecordSerializer.ExtraMeta.non_futures + [
+            "collection_month",
+            "received_month",
+            "submission_date",
+        ]
