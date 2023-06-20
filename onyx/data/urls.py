0e143510756a2955a0a54f276e891b643308dc10
tombch <t.brier@outlook.com>
2023-06-20 15:08:44 +0100

    Names for data urls

--- onyx/data/urls.py
+++ onyx/data/urls.py
@@ -5,15 +5,18 @@ urlpatterns = [
     path(
         "create/<code>/",
         views.CreateRecordView.as_view(),
+        name="data.create",
     ),
     path(
         "testcreate/<code>/",
         views.CreateRecordView.as_view(),
         kwargs={"test": True},
+        name="data.testcreate",
     ),
     path(
         "get/<code>/<cid>/",
         views.GetRecordView.as_view(),
+        name="data.get",
     ),
     path(
         "filter/<code>/",
@@ -22,36 +25,44 @@ urlpatterns = [
     path(
         "query/<code>/",
         views.QueryRecordView.as_view(),
+        name="data.filter",
     ),
     path(
         "update/<code>/<cid>/",
         views.UpdateRecordView.as_view(),
+        name="data.update",
     ),
     path(
         "testupdate/<code>/<cid>/",
         views.UpdateRecordView.as_view(),
         kwargs={"test": True},
+        name="data.testupdate",
     ),
     path(
         "suppress/<code>/<cid>/",
         views.SuppressRecordView.as_view(),
+        name="data.suppress",
     ),
     path(
         "testsuppress/<code>/<cid>/",
         views.SuppressRecordView.as_view(),
         kwargs={"test": True},
+        name="data.testsuppress",
     ),
     path(
         "delete/<code>/<cid>/",
         views.DeleteRecordView.as_view(),
+        name="data.delete",
     ),
     path(
         "testdelete/<code>/<cid>/",
         views.DeleteRecordView.as_view(),
         kwargs={"test": True},
+        name="data.testdelete",
     ),
     path(
         "choices/<code>/<field>/",
         views.ChoicesView.as_view(),
+        name="data.choices",
     ),
 ]
