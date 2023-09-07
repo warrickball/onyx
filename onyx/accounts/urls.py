fc8c90412abc9224b41697f5d9c5917633d5e102
tombch <t.brier@outlook.com>
2023-09-07 15:40:40 +0100

    Restructured url patterns to move prefixes onto apps

--- onyx/accounts/urls.py
+++ onyx/accounts/urls.py
@@ -1,18 +1,32 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from knox import views as knox_views
 
 urlpatterns = [
-    path("register/", views.CreateUserView.as_view()),
-    path("login/", views.LoginView.as_view(), name="knox_login"),
-    path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
-    path("logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
-    path("site/approve/<username>/", views.SiteApproveView.as_view()),
-    path("site/waiting/", views.SiteWaitingView.as_view()),
-    path("site/users/", views.SiteUsersView.as_view()),
-    path("admin/approve/<username>/", views.AdminApproveView.as_view()),
-    path("admin/waiting/", views.AdminWaitingView.as_view()),
-    path("admin/users/", views.AdminUsersView.as_view()),
-    path("admin/projects/<username>/", views.AdminUserProjectsView.as_view()),
-    path("admin/projectuser/<code>/<username>/", views.CreateProjectUserView.as_view()),
+    path("accounts/register/", views.CreateUserView.as_view()),
+    path("accounts/login/", views.LoginView.as_view(), name="knox_login"),
+    path("accounts/logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
+    path(
+        "accounts/logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"
+    ),
+    re_path(
+        r"^accounts/site/approve/(?P<username>[a-zA-Z_]*)/$",
+        views.SiteApproveView.as_view(),
+    ),
+    path("accounts/site/waiting/", views.SiteWaitingView.as_view()),
+    path("accounts/site/users/", views.SiteUsersView.as_view()),
+    re_path(
+        r"^accounts/admin/approve/(?P<username>[a-zA-Z_]*)/$",
+        views.AdminApproveView.as_view(),
+    ),
+    path("accounts/admin/waiting/", views.AdminWaitingView.as_view()),
+    path("accounts/admin/users/", views.AdminUsersView.as_view()),
+    re_path(
+        r"^control/projectgroups/(?P<username>[a-zA-Z_]*)/$",
+        views.ControlProjectGroupsView.as_view(),
+    ),
+    re_path(
+        r"^control/projectuser/(?P<code>[a-zA-Z_]*)/(?P<username>[a-zA-Z_]*)/$",
+        views.ControlProjectUserView.as_view(),
+    ),
 ]
--- onyx/data/urls.py
+++ onyx/data/urls.py
@@ -4,45 +4,45 @@ from . import views
 
 urlpatterns = [
     path(
-        "",
+        "projects/",
         views.ProjectsView.as_view(),
         name="data.project.list",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/fields/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/fields/$",
         views.FieldsView.as_view(),
         name="data.project.fields",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/choices/(?P<field>[a-zA-Z_]*)/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/choices/(?P<field>[a-zA-Z_]*)/$",
         views.ChoicesView.as_view(),
         name="data.project.choices",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/$",
         views.ProjectRecordsViewSet.as_view({"post": "create", "get": "list"}),
         name="data.project",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/(?P<cid>[cC]-[a-zA-Z0-9]{10})/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/(?P<cid>[cC]-[a-zA-Z0-9]{10})/$",
         views.ProjectRecordsViewSet.as_view(
             {"get": "retrieve", "patch": "partial_update", "delete": "destroy"}
         ),
         name="data.project.cid",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/query/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/query/$",
         views.ProjectRecordsViewSet.as_view({"post": "query"}),
         name="data.project.query",
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/test/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/test/$",
         views.ProjectRecordsViewSet.as_view({"post": "create"}),
         name="data.project.test",
         kwargs={"test": True},
     ),
     re_path(
-        r"^(?P<code>[a-zA-Z_]*)/test/(?P<cid>[cC]-[a-zA-Z0-9]{10})/$",
+        r"^projects/(?P<code>[a-zA-Z_]*)/test/(?P<cid>[cC]-[a-zA-Z0-9]{10})/$",
         views.ProjectRecordsViewSet.as_view({"patch": "partial_update"}),
         name="data.project.test.cid",
         kwargs={"test": True},
