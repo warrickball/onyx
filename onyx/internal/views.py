6e6f431e0b93c5c294d59b8d071c24bfd68d625d
tombch <t.brier@outlook.com>
2023-10-06 11:46:52 +0100

    Better custom error views

--- onyx/internal/views.py
+++ onyx/internal/views.py
@@ -1,61 +1,46 @@
-import json
-from django.http import HttpResponse
+from django.http import JsonResponse
 from rest_framework import status
 
 
-def custom_page_not_found_view(request, exception):
-    return HttpResponse(
-        content=json.dumps(
-            {
-                "status": "fail",
-                "code": status.HTTP_404_NOT_FOUND,
-                "messages": {"detail": "Not found."},
-            }
-        ),
-        content_type="application/json",
+def custom_page_not_found_view(*args, **kwargs):
+    return JsonResponse(
+        {
+            "status": "fail",
+            "code": status.HTTP_404_NOT_FOUND,
+            "messages": {"detail": "Not found."},
+        },
         status=status.HTTP_404_NOT_FOUND,
     )
 
 
-def custom_error_view(request, exception=None):
-    return HttpResponse(
-        content=json.dumps(
-            {
-                "status": "error",
-                "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-                "messages": {
-                    "detail": "Internal server error. Please contact an admin."
-                },
-            }
-        ),
-        content_type="application/json",
+def custom_error_view(*args, **kwargs):
+    return JsonResponse(
+        {
+            "status": "error",
+            "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "messages": {"detail": "Internal server error. Please contact an admin."},
+        },
         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
 
 
-def custom_permission_denied_view(request, exception=None):
-    return HttpResponse(
-        content=json.dumps(
-            {
-                "status": "fail",
-                "code": status.HTTP_403_FORBIDDEN,
-                "messages": {"detail": "Permission denied."},
-            }
-        ),
-        content_type="application/json",
+def custom_permission_denied_view(*args, **kwargs):
+    return JsonResponse(
+        {
+            "status": "fail",
+            "code": status.HTTP_403_FORBIDDEN,
+            "messages": {"detail": "Permission denied."},
+        },
         status=status.HTTP_403_FORBIDDEN,
     )
 
 
-def custom_bad_request_view(request, exception=None):
-    return HttpResponse(
-        content=json.dumps(
-            {
-                "status": "fail",
-                "code": status.HTTP_400_BAD_REQUEST,
-                "messages": {"detail": "Bad request."},
-            }
-        ),
-        content_type="application/json",
+def custom_bad_request_view(*args, **kwargs):
+    return JsonResponse(
+        {
+            "status": "fail",
+            "code": status.HTTP_400_BAD_REQUEST,
+            "messages": {"detail": "Bad request."},
+        },
         status=status.HTTP_400_BAD_REQUEST,
     )
