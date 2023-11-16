c64547a7435b66924eb195ea2241b14f7f1da54a
Thomas Brier <46268349+tombch@users.noreply.github.com>
2023-11-16 19:12:35 +0000

    v0.7.0: Moved to Poetry for managing dependancies, removed python-3.10 from tests (#64)
    * Trying out poetry
    
    * Updated tests to work with poetry
    
    * Updated workflow to exclude python 3.10 as its below allowed versions (might change this later)
    
    * Fixed django test workflow
    
    * Updated workflow to install poetry with pipx
    
    * Removed need for non-existent logs folder on setup. Just write logs to project working dir
    
    * removed requirements.txt and test-requirements.txt, deleted setup script as its not doing much and modified start/stop scripts to use poetry
    
    * Renamed and updated test workflow to install pipx same as prod

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,13 +2,6 @@
 cd "${0%/*}"
 ./stop.sh
 cd ..
-
-echo "Activating Python virtual environment..."
-source .venv/bin/activate
-
-cd onyx/
-
 echo "Starting Gunicorn server..."
-gunicorn -c onyx.gunicorn.py
-
+poetry run gunicorn -c onyx/onyx.gunicorn.py
 echo "Onyx started."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 echo "Stopping Gunicorn server..."
 pkill -f "onyx.gunicorn.py"
-
 echo "Onyx stopped."
