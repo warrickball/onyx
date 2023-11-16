32d7f2464d8f3b3fb54368ca7f778b6c164726eb
tombch <t.brier@outlook.com>
2023-11-16 18:11:03 +0000

    Removed need for non-existent logs folder on setup. Just write logs to project working dir

--- onyx/onyx.gunicorn.py
+++ onyx/onyx.gunicorn.py
@@ -13,12 +13,12 @@ wsgi_app = "onyx.wsgi"
 bind = os.environ["GUNICORN_BIND"]
 workers = os.environ["GUNICORN_WORKERS"]
 
-accesslog = os.path.join(chdir, "../logs", "access.log")
+accesslog = os.path.join(chdir, "../access.log")
 access_log_format = (
     '%(t)s %({x-forwarded-for}i)s %(u)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(M)s ms'
 )
 
-errorlog = os.path.join(chdir, "../logs", "error.log")
+errorlog = os.path.join(chdir, "../error.log")
 capture_output = True  # Redirect stdout/stderr to errorlog
 
 daemon = True  # Run process in the background
