298732a027fcfdc7d4109d7ed3f0792e119d9db2
tombch <t.brier@outlook.com>
2024-01-08 14:11:08 +0000

    Ordering and non-future constraints

--- onyx/utils/constraints.py
+++ onyx/utils/constraints.py
@@ -1,17 +1,21 @@
 import functools
 import operator
+from datetime import datetime
 from django.db import models
 from django.db.models import Q
 
 
-def unique_together(model_name, fields):
+# TODO: Test constraints
+
+
+def unique_together(model_name: str, fields: list[str]):
     return models.UniqueConstraint(
         fields=fields,
         name=f"unique_together_{model_name}_{'_'.join(fields)}",
     )
 
 
-def optional_value_group(model_name, fields):
+def optional_value_group(model_name: str, fields: list[str]):
     return models.CheckConstraint(
         check=functools.reduce(
             operator.or_, [Q(**{f"{field}__isnull": False}) for field in fields]
@@ -23,7 +27,37 @@ def optional_value_group(model_name, fields):
     )
 
 
-def conditional_required(model_name, field, required):
+def ordering(model_name: str, fields: tuple[str, str]):
+    lower, higher = fields
+    return models.CheckConstraint(
+        check=(
+            models.Q(**{f"{lower}__isnull": True})
+            | models.Q(**{f"{higher}__isnull": True})
+            | models.Q(**{f"{lower}__lte": models.F(higher)})
+        ),
+        name=f"ordering_{model_name}_{lower}_{higher}",
+        violation_error_message=f"The '{lower}' must be less than or equal to '{higher}'.",
+    )
+
+
+def non_futures(model_name: str, fields: list[str]):
+    return models.CheckConstraint(
+        check=functools.reduce(
+            operator.and_,
+            [
+                Q(**{f"{field}__isnull": True})
+                | ~Q(**{f"{field}__gt": datetime.now().date()})
+                for field in fields
+            ],
+        ),
+        name=f"non_future_{model_name}_{'_'.join(fields)}",
+        violation_error_message="At least one of '"
+        + "', '".join(fields)
+        + "' is from the future.",
+    )
+
+
+def conditional_required(model_name: str, field: str, required: list[str]):
     return models.CheckConstraint(
         check=Q(**{f"{field}__isnull": True})
         | functools.reduce(
