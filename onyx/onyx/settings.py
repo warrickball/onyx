4ad25fc21427e1dc9ce3a752d7b2a17ab5ee79c2
tombch <t.brier@outlook.com>
2024-01-16 14:25:39 +0000

    Github actions test with postgres

--- onyx/onyx/settings.py
+++ onyx/onyx/settings.py
@@ -96,8 +96,11 @@ WSGI_APPLICATION = "onyx.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["DATABASE_HOST"],
+        "PORT": os.environ["DATABASE_PORT"],
         "NAME": os.environ["DATABASE_NAME"],
         "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
     }
 }
 
