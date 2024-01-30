14e2b6a04260778fcc757c18f0fbaddbf9df21a7
Thomas Brier <46268349+tombch@users.noreply.github.com>
2024-01-30 15:03:38 +0000

    Update settings.py
    Minor change to mirror how the sqlite db was set up previously

--- onyx/onyx/settings.py
+++ onyx/onyx/settings.py
@@ -104,14 +104,12 @@ DATABASES = {
 # In CI, Django has to connect differently to the Postgres container,
 # and we have to provide a host name and password
 if os.environ.get("CI") and "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ["DATABASE_NAME"],
-            "USER": os.environ["DATABASE_USER"],
-            "PASSWORD": os.environ["DATABASE_PASSWORD"],
-            "HOST": os.environ["DATABASE_HOST"],
-        }
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ["DATABASE_HOST"],
     }
 
 # Password validation
