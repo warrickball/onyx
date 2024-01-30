914f67775041f9c4a23d8ca501ebe0d89c511d79
Thomas Brier <46268349+tombch@users.noreply.github.com>
2024-01-30 14:46:20 +0000

    Merge development updates (#98)
    * Refactor of project config command to have its input JSON defined by pydantic models (#85)
    
    * Restructuring of project JSON, and update to management command to accomodate
    
    * Pydantic validation of project config JSON
    
    * Fix bug in generate_fields_spec created during incorrect resolve of merge conflict
    
    * Merge dependency updates (#92)
    
    * Bump python-dotenv from 1.0.0 to 1.0.1 (#89)
    
    Bumps [python-dotenv](https://github.com/theskumar/python-dotenv) from 1.0.0 to 1.0.1.
    - [Release notes](https://github.com/theskumar/python-dotenv/releases)
    - [Changelog](https://github.com/theskumar/python-dotenv/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/theskumar/python-dotenv/compare/v1.0.0...v1.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: python-dotenv
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump coverage from 7.4.0 to 7.4.1 (#88)
    
    Bumps [coverage](https://github.com/nedbat/coveragepy) from 7.4.0 to 7.4.1.
    - [Release notes](https://github.com/nedbat/coveragepy/releases)
    - [Changelog](https://github.com/nedbat/coveragepy/blob/master/CHANGES.rst)
    - [Commits](https://github.com/nedbat/coveragepy/compare/7.4.0...7.4.1)
    
    ---
    updated-dependencies:
    - dependency-name: coverage
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Version bump
    
    * Merge master back into development (#94)
    
    * Bump python-dotenv from 1.0.0 to 1.0.1 (#89)
    
    Bumps [python-dotenv](https://github.com/theskumar/python-dotenv) from 1.0.0 to 1.0.1.
    - [Release notes](https://github.com/theskumar/python-dotenv/releases)
    - [Changelog](https://github.com/theskumar/python-dotenv/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/theskumar/python-dotenv/compare/v1.0.0...v1.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: python-dotenv
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump coverage from 7.4.0 to 7.4.1 (#88)
    
    Bumps [coverage](https://github.com/nedbat/coveragepy) from 7.4.0 to 7.4.1.
    - [Release notes](https://github.com/nedbat/coveragepy/releases)
    - [Changelog](https://github.com/nedbat/coveragepy/blob/master/CHANGES.rst)
    - [Commits](https://github.com/nedbat/coveragepy/compare/7.4.0...7.4.1)
    
    ---
    updated-dependencies:
    - dependency-name: coverage
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Fix non-future constraint, added missing constraints on test models (#97)
    
    * Identified bug with non-future constraint
    
    * Fixed non_futures constraint bug, and added missing db-level constraints in test model
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- onyx/utils/constraints.py
+++ onyx/utils/constraints.py
@@ -1,14 +1,26 @@
 import functools
 import operator
-from datetime import datetime
 from django.db import models
-from django.db.models import Q
+from django.db.models import F, Q
 
 
 # TODO: Test constraints
 
 
 def unique_together(model_name: str, fields: list[str]):
+    """
+    Creates a unique constraint over the provided `fields`.
+
+    This means that the combination of these fields in a given instance must be unique across all other instances.
+
+    Args:
+        model_name: The name of the model (used in naming the constraint).
+        fields: The fields to create the constraint over.
+
+    Returns:
+        The constraint.
+    """
+
     return models.UniqueConstraint(
         fields=fields,
         name=f"unique_together_{model_name}_{'_'.join(fields)}",
@@ -16,10 +28,26 @@ def unique_together(model_name: str, fields: list[str]):
 
 
 def optional_value_group(model_name: str, fields: list[str]):
+    """
+    Creates a constraint that ensures at least one of the provided `fields` is not null.
+
+    Args:
+        model_name: The name of the model (used in naming the constraint).
+        fields: The fields to create the constraint over.
+
+    Returns:
+        The constraint.
+    """
+
+    # For each field, build a Q object that requires the field is not null
+    q_objects = [Q(**{f"{field}__isnull": False}) for field in fields]
+
+    # Reduce the Q objects into a single Q object that requires at least one of the fields is not null
+    # This is done by OR-ing the Q objects together
+    check = functools.reduce(operator.or_, q_objects)
+
     return models.CheckConstraint(
-        check=functools.reduce(
-            operator.or_, [Q(**{f"{field}__isnull": False}) for field in fields]
-        ),
+        check=check,
         name=f"optional_value_group_{model_name}_{'_'.join(fields)}",
         violation_error_message="At least one of '"
         + "', '".join(fields)
@@ -28,28 +56,63 @@ def optional_value_group(model_name: str, fields: list[str]):
 
 
 def ordering(model_name: str, fields: tuple[str, str]):
+    """
+    Creates a constraint that ensures the first field is less than or equal to the second field.
+
+    Args:
+        model_name: The name of the model (used in naming the constraint).
+        fields: The fields to create the constraint over.
+
+    Returns:
+        The constraint.
+    """
+
+    # Split the fields tuple into lower and higher
     lower, higher = fields
+
+    # Build a Q object that requires that either:
+    # - One of the two fields is null
+    # - The lower field is less than or equal to the higher field
+    check = (
+        models.Q(**{f"{lower}__isnull": True})
+        | models.Q(**{f"{higher}__isnull": True})
+        | models.Q(**{f"{lower}__lte": models.F(higher)})
+    )
+
     return models.CheckConstraint(
-        check=(
-            models.Q(**{f"{lower}__isnull": True})
-            | models.Q(**{f"{higher}__isnull": True})
-            | models.Q(**{f"{lower}__lte": models.F(higher)})
-        ),
+        check=check,
         name=f"ordering_{model_name}_{lower}_{higher}",
         violation_error_message=f"The '{lower}' must be less than or equal to '{higher}'.",
     )
 
 
 def non_futures(model_name: str, fields: list[str]):
+    """
+    Creates a constraint that ensures that the provided `fields` are not from the future.
+
+    Args:
+        model_name: The name of the model (used in naming the constraint).
+        fields: The fields to create the constraint over.
+
+    Returns:
+        The constraint.
+    """
+
+    # For each field, build a Q object that requires the field is null or less than or equal to the last_modified field
+    q_objects = [
+        Q(**{f"{field}__isnull": True}) | Q(**{f"{field}__lte": F("last_modified")})
+        for field in fields
+    ]
+
+    # Reduce the Q objects into a single Q object that requires all of the fields are not from the future
+    # This is done by AND-ing the Q objects together
+    check = functools.reduce(
+        operator.and_,
+        q_objects,
+    )
+
     return models.CheckConstraint(
-        check=functools.reduce(
-            operator.and_,
-            [
-                Q(**{f"{field}__isnull": True})
-                | ~Q(**{f"{field}__gt": datetime.now().date()})
-                for field in fields
-            ],
-        ),
+        check=check,
         name=f"non_future_{model_name}_{'_'.join(fields)}",
         violation_error_message="At least one of '"
         + "', '".join(fields)
@@ -58,11 +121,31 @@ def non_futures(model_name: str, fields: list[str]):
 
 
 def conditional_required(model_name: str, field: str, required: list[str]):
+    """
+    Creates a constraint that ensures that the provided `field` must be null unless all of the `required` fields are not null.
+
+    Args:
+        model_name: The name of the model (used in naming the constraint).
+        field: The field to create the constraint over.
+        required: The fields that are required in order to set the `field`.
+
+    Returns:
+        The constraint.
+    """
+
+    # For each required field, build a Q object that requires the field is not null
+    q_objects = [Q(**{f"{req}__isnull": False}) for req in required]
+
+    # Reduce the Q objects into a single Q object that requires all of the required fields are not null
+    requirements = functools.reduce(operator.and_, q_objects)
+
+    # Build a Q object that requires that either:
+    # - The field is null
+    # - All of the required fields are not null
+    check = Q(**{f"{field}__isnull": True}) | requirements
+
     return models.CheckConstraint(
-        check=Q(**{f"{field}__isnull": True})
-        | functools.reduce(
-            operator.and_, [Q(**{f"{req}__isnull": False}) for req in required]
-        ),
+        check=check,
         name=f"conditional_required_{model_name}_{field}_requires_{'_'.join(required)}",
         violation_error_message="All of '"
         + "', '".join(required)
